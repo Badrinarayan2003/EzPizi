0314ee24aa0e7443ad6685a1dfdff0c18fcd5a15
Badrinarayan2003 <badrithamba@gmail.com>
2024-04-10 13:07:53 +0530

    remove extra div from from loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -8,27 +8,19 @@ function Loading() {
         <>
             <div className="col-md-4 col-xl-3 col-md-4 col-sm-6 col-6 mb-2">
                 <div className="card border-0 img-box" style={{ height: '410px', backgroundColor: '#8a8684' }}>
-                    <div className="card-img-top" style={{ backgroundColor: 'rgb(216, 216, 216)' }}></div>
-                    <div className="card-body product-detail-box"></div>
-                </div> 
-            </div>
-            <div className="col-lg-4 col-xl-3 col-md-4 col-sm-6 col-6 mb-2">
-                <div className="card border-0 img-box" style={{ height: '410px', backgroundColor: '#8a8684' }}>
-                    <div className="card-img-top" style={{ backgroundColor: 'rgb(216, 216, 216)' }}></div>
-                    <div className="card-body product-detail-box"></div>
                 </div>
             </div>
             <div className="col-lg-4 col-xl-3 col-md-4 col-sm-6 col-6 mb-2">
                 <div className="card border-0 img-box" style={{ height: '410px', backgroundColor: '#8a8684' }}>
-                    <div className="card-img-top" style={{ backgroundColor: 'rgb(216, 216, 216)' }}></div>
-                    <div className="card-body product-detail-box"></div>
+                </div>
+            </div>
+            <div className="col-lg-4 col-xl-3 col-md-4 col-sm-6 col-6 mb-2">
+                <div className="card border-0 img-box" style={{ height: '410px', backgroundColor: '#8a8684' }}>
                 </div>
 
             </div>
             <div className="col-lg-4 col-xl-3 col-md-4 col-sm-6 col-6 mb-2">
                 <div className="card border-0 img-box" style={{ height: '410px', backgroundColor: '#8a8684' }}>
-                    <div className="card-img-top" style={{ backgroundColor: 'rgb(216, 216, 216)' }}></div>
-                    <div className="card-body product-detail-box"></div>
                 </div>
             </div>
         </>
